94d57462a0f0cf8fd5b187cbff25aece18846f29
trt14 <tarooti14dev@gmail.com>
2022-01-12 09:26:59 +0300

    Initial Commit

---
+++ MyFinalExam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFinalExam
+//
+//  Created by Tarooti on 09/06/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
